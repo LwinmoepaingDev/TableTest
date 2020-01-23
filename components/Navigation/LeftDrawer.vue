4b49f3f20d08c2caf7dcb6ad4ba06a9ae6004a56
Lwin Moe Paing <lwinmoepaing@marathonmyanmar.com>
2020-01-23 12:33:57 +0630

    First Commit

---
+++ components/Navigation/LeftDrawer.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-navigation-drawer
+    v-model="propDrawer"
+    :mini-variant="miniVariant"
+    :clipped="clipped"
+    fixed
+    app
+  >
+    <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: {
+    items: Array,
+    miniVariant: Boolean,
+    clipped: Boolean,
+    fixed: Boolean,
+    drawer: {
+      type: Boolean,
+      require: true
+    }
+  },
+  computed: {
+    propDrawer: {
+      get() { return this.drawer },
+      set(propDrawer) {
+        this.$emit('drawerToggle', propDrawer);
+      }
+    }
+  }
+}
+</script>
---
+++ layouts/master.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-app dark>
+    <LeftDrawer
+      :drawer="drawer"
+      :items="items"
+      :miniVariant="miniVariant"
+      :clipped="clipped"
+      :fixed="fixed"
+      @drawerToggle="setDrawer($event)"
+    />
+
+    <AppBar
+      :miniVariant="miniVariant"
+      :clipped="clipped"
+      :title="title"
+      @drawerToggle="drawer = !drawer"
+      @miniVariantToggle="miniVariant = !miniVariant"
+      @clippedToggle="clipped = !clipped"
+      @fixedToggle="fixed = !fixed"
+      @rightDrawerToggle="rightDrawer = !rightDrawer"
+    />
+
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <RightNavigationDrawer
+      :fixed="fixed"
+      :rightDrawer="rightDrawer"
+      @drawerToggle="setRightDrawer($event)"
+    />
+
+    <Footer :fixed="fixed" />
+  </v-app>
+</template>
+
+<script>
+import AppBar from '~/components/Navigation/AppBar'
+import LeftDrawer from '~/components/Navigation/LeftDrawer'
+import RightNavigationDrawer from '~/components/Navigation/RightNavigationDrawer'
+import Footer from '~/components/Footer'
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Master'
+    }
+  },
+  methods: {
+    setDrawer (event) { this.drawer = event },
+    setRightDrawer (event) { this.rightDrawer = event}
+  },
+  components: { AppBar, Footer, LeftDrawer, RightNavigationDrawer }
+}
+</script>
